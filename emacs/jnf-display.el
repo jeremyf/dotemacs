15bcfa2ddc0dfba2e90826bf25d1f8df7c873ad6
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-08-10 14:22:25 -0400

    Favoring spaceline over powerline
    At this point, I favor the structure of the default spaceline.  When I
    have spaceline all of the icons running, it's just a tick to slow for my
    preference.  I wish I could enable it, but alas, no go.

--- emacs/jnf-display.el
+++ emacs/jnf-display.el
@@ -18,7 +18,6 @@
 
 (require 'jnf-modus-main.el)
 
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; BEGIN BLOCK
 ;;
@@ -56,72 +55,15 @@
 ;;   :after all-the-icons
 ;;   :hook (dired-mode . all-the-icons-dired-mode))
 
-(defun jnf/powerline-theme ()
-  "Setup the default mode-line."
-  (interactive)
-  (setq-default mode-line-format
-                '("%e"
-                  (:eval
-                   (let* ((active (powerline-selected-window-active))
-                          (mode-line-buffer-id (if active 'mode-line-buffer-id 'mode-line-buffer-id-inactive))
-                          (mode-line (if active 'mode-line 'mode-line-inactive))
-                          (face0 (if active 'powerline-active0 'powerline-inactive0))
-                          (face1 (if active 'powerline-active1 'powerline-inactive1))
-                          (face2 (if active 'powerline-active2 'powerline-inactive2))
-                          (separator-left (intern (format "powerline-%s-%s"
-                                                          (powerline-current-separator)
-                                                          (car powerline-default-separator-dir))))
-                          (separator-right (intern (format "powerline-%s-%s"
-                                                           (powerline-current-separator)
-                                                           (cdr powerline-default-separator-dir))))
-                          (lhs (list (powerline-raw "%*" face0 'l)
-                                     (powerline-raw "%4l" face0 'l)
-                                     (powerline-raw ":" face0 'l)
-                                     (powerline-raw "%3c" face0 'r)
-                                     (powerline-raw " " face0)
-                                     (powerline-raw "%6p" face0 'r)
-                                     ;; Commenting out the heads up display of relative position.
-                                     ;; (when powerline-display-hud
-                                     ;;   (powerline-hud face2 face0))
-                                     ;; I rarely care about buffer size, so let's not worry about this.
-                                     ;; (when powerline-display-buffer-size
-                                     ;;   (powerline-buffer-size face0 'l))
-                                     ;; (when powerline-display-mule-info
-                                     ;;   (powerline-raw mode-line-mule-info face0 'l))
-                                     (powerline-buffer-id `(mode-line-buffer-id ,face0) 'l)
-                                     (when (and (boundp 'which-func-mode) which-func-mode)
-                                       (powerline-raw which-func-format face0 'l))
-                                     (powerline-raw " " face0)
-                                     (when (and (boundp 'erc-track-minor-mode) erc-track-minor-mode)
-                                       (powerline-raw erc-modified-channels-object face0 'l))
-                                     (powerline-major-mode face0 'l)
-                                     (powerline-process face0)
-                                     (powerline-vc face0 'r)
-                                     (powerline-minor-modes face0 'l)
-                                     (powerline-narrow face0 'l)
-                                     (powerline-raw " " face0)
-                                     (when (bound-and-true-p nyan-mode)
-                                       (powerline-raw (list (nyan-create)) face0 'l))))
-                          (rhs (list (powerline-raw global-mode-string face0 'r)
-                                     (unless window-system
-                                       (powerline-raw (char-to-string #xe0a1) face0 'l))
-                                     (when powerline-display-hud
-                                       (powerline-hud face0 face0))
-                                     (powerline-fill face0 0)
-                                     )))
-                     (concat (powerline-render lhs)
-                             (powerline-fill face0 (powerline-width rhs))
-                             (powerline-render rhs)))))))
-
-(use-package powerline
-  :straight (powerline :type git :host github :repo "milkypostman/powerline")
-  :config (jnf/powerline-theme))
+;; (use-package powerline
+;;   :straight (powerline :type git :host github :repo "milkypostman/powerline")
+;;   :config (powerline-default-theme))
 
 ;; A nice looking modeline enhancement
-;; (use-package spaceline
-  ;; :straight t)
+(use-package spaceline
+  :straight t)
 
-;; ;; Add some visual flair to the modeline enhancements
+;; Add some visual flair to the modeline enhancements
 ;; (use-package spaceline-all-the-icons
 ;;   :straight t
 ;;   :after spaceline
@@ -130,11 +72,17 @@
 
 ;; A convenience function to create a nice string
 (defun with-faicon (icon str &optional height v-adjust)
-    "Displays an icon from Font Awesome icon."
+    "Displays an ICON  from Font Awesome icon.
+
+The STR identifies the icon and the HEIGHT and V-ADJUST provide
+the configuration."
     (s-concat (all-the-icons-faicon icon :v-adjust (or v-adjust 0) :height (or height 1)) " " str))
 
 (defun with-material (icon str &optional height v-adjust)
-    "Displays an icon from Font Material icon."
+    "Displays an ICON  from Font Material icon.
+
+The STR identifies the icon and the HEIGHT and V-ADJUST provide
+the configuration."
     (s-concat (all-the-icons-material icon :v-adjust (or v-adjust 0) :height (or height 1)) " " str))
 
 (provide 'jnf-display.el)
