e8404ca7a483d70b3af89628b258c83adb8693ad
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-08-12 17:33:40 -0400

    Adding projectile file for configuration

---
+++ emacs/jnf-projectile.el
@@ -0,0 +1,24 @@
+;;; jnf-projectile.el --- Summary -*- lexical-binding: t; -*-
+;;;
+;;; Commentary:
+;;;
+;;; Code:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; https://docs.projectile.mx/en/latest/
+;;
+;; Helpful for understanding the likely bounds of directory structure
+(use-package projectile
+  :straight t
+  :diminish 'projectile-mode
+  :config
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (projectile-mode +1)
+  :custom
+  (projectile-project-search-path '("~/git/"))
+  :bind (("s-t" . projectile-find-file)
+         ("s-." . projectile-toggle-between-implementation-and-test)))
+
+
+(provide 'jnf-projectile.el)
+;;; jnf-projectile.el ends here
