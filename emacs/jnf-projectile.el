7c6763e192e1543f2d5484adc3aa33efcf4fe53b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-08-13 06:53:28 -0400

    Updating projectile configuration
    This morning I spent some time exploring projectile, these changes
    provides just a bit more syntactic sugar.

--- emacs/jnf-projectile.el
+++ emacs/jnf-projectile.el
@@ -12,14 +12,17 @@
   :straight t
   :diminish 'projectile-mode
   :config
-  ;; (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   (projectile-mode +1)
   :custom
   (projectile-project-search-path '("~/git/"))
-  :bind (:map projectile-mode-map ("C-c p" . projectile-command-map)
-         ("s-t" . projectile-find-file)
-         ("s-." . projectile-toggle-between-implementation-and-test)))
-
+  :bind
+  (:map projectile-mode-map (
+                             ("C-s-p" . projectile-command-map)
+                             ("C-c p" . projectile-command-map)
+                             ))
+  :bind
+  ("s-t" . projectile-find-file-dwim)
+  ("s-." . projectile-toggle-between-implementation-and-test))
 
 (provide 'jnf-projectile.el)
 ;;; jnf-projectile.el ends here
