d4b6df3dc401ca99e9356f270df7d7a0472c4f8b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-08-30 13:01:09 -0400

    Fixing broken default local hostname
    file-truename does not apply to an HTTP url

--- jnf-emacs-packages/jnf-config.el
+++ jnf-emacs-packages/jnf-config.el
@@ -33,7 +33,7 @@ Alternatives:
   "The home directory of TakeOnRules.com Hugo repository.")
 
 (defconst jnf/tor-default-local-hostname
-  (file-truename "http://localhost:1313")
+  "http://localhost:1313"
   "The scheme, host name, and port for serving up a local TakeOnRules.com.")
 
 (defvar jnf/data-directories
