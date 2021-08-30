67909a3ea13d36a395c0b8f52d326e6ea26c1f1d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-08-30 13:21:24 -0400

    Extracting and reworking display functions

--- jnf-emacs-packages/jnf-display.el
+++ jnf-emacs-packages/jnf-display.el
@@ -42,6 +42,28 @@
 
 ;; Nice for neotree
 (use-package all-the-icons
+  :config
+  ;; A convenience function to create a nice string
+  (defun with-faicon (icon str &optional height v-adjust)
+    "Displays an ICON  from Font Awesome icon.
+
+The STR identifies the icon and the HEIGHT and V-ADJUST provide
+the configuration."
+    (s-concat (all-the-icons-faicon icon :v-adjust (or v-adjust 0) :height (or height 1)) " " str))
+
+  (defun with-material (icon str &optional height v-adjust)
+    "Displays an ICON  from Font Material icon.
+
+The STR identifies the icon and the HEIGHT and V-ADJUST provide
+the configuration."
+    (s-concat (all-the-icons-material icon :v-adjust (or v-adjust 0) :height (or height 1)) " " str))
+
+  (defun with-octicon (icon str &optional height v-adjust)
+    "Displays an ICON  from Octicon icon.
+
+The STR identifies the icon and the HEIGHT and V-ADJUST provide
+the configuration."
+    (s-concat (all-the-icons-octicon icon :v-adjust (or v-adjust 0) :height (or height 1)) " " str))
   :straight t)
 
 ;; Disabled because on 2021-04-11 I got the following error:
@@ -67,28 +89,57 @@
 ;;   :after spaceline
 ;;   :config (spaceline-all-the-icons-theme))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; BEGIN frame and window quick setup
+(defun gk-layouts-3col ()
+  "Three column layout.
 
-;; A convenience function to create a nice string
-(defun with-faicon (icon str &optional height v-adjust)
-    "Displays an ICON  from Font Awesome icon.
+Tries to preserve the order of window buffers and active window."
+  (interactive)
+  ;; Record active window buffer.
+  (let ((cbuf (current-buffer)))
+    ;; Switch to leftmost window.
+    (ignore-errors (cl-loop do (windmove-left)))
+    (let ((buffers
+           (mapcar #'window-buffer (-take 3 (window-list))))
+          (width (/ (frame-width) 3)))
+      (delete-other-windows)
+      (split-window-horizontally width)
+      (other-window 1)
+      (split-window-horizontally)
+      (other-window -1)
+      (dolist (b buffers)
+        (switch-to-buffer b)
+        (other-window 1)))
+    ;; Switch to previously visible buffer’s window.
+    (select-window (get-buffer-window cbuf))))
 
-The STR identifies the icon and the HEIGHT and V-ADJUST provide
-the configuration."
-    (s-concat (all-the-icons-faicon icon :v-adjust (or v-adjust 0) :height (or height 1)) " " str))
 
-(defun with-material (icon str &optional height v-adjust)
-    "Displays an ICON  from Font Material icon.
+(defun gk-layouts-main-and-sidekicks ()
+  "One horizontal split, the right window split in two.
 
-The STR identifies the icon and the HEIGHT and V-ADJUST provide
-the configuration."
-    (s-concat (all-the-icons-material icon :v-adjust (or v-adjust 0) :height (or height 1)) " " str))
+Tries to preserve the order of window buffers and active window."
+  (interactive)
+  ;; Record active window buffer.
+  (let ((cbuf (current-buffer)))
+    ;; Switch to leftmost window.
+    (ignore-errors (cl-loop do (windmove-left)))
+    (let ((buffers
+           (mapcar #'window-buffer (-take 3 (window-list)))))
+      (delete-other-windows)
+      (split-window-horizontally)
+      (other-window 1)
+      (split-window-vertically)
+      (other-window -1)
+      (dolist (b buffers)
+        (switch-to-buffer b)
+        (other-window 1)))
+    ;; Switch to previously visible buffer’s window.
+    (select-window (get-buffer-window cbuf))))
 
-(defun with-octicon (icon str &optional height v-adjust)
-    "Displays an ICON  from Octicon icon.
-
-The STR identifies the icon and the HEIGHT and V-ADJUST provide
-the configuration."
-    (s-concat (all-the-icons-octicon icon :v-adjust (or v-adjust 0) :height (or height 1)) " " str))
+(bind-key "C-x \\" #'gk-layouts-main-and-sidekicks)
+;; END frame and window quick setup
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (provide 'jnf-display.el)
 ;;; jnf-display.el ends here
