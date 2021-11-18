e15a9a1059447ede1a38342d2e8b5f30fb22147f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-11-18 08:32:54 -0500

    Favoring a Hydra menu for base projectile

--- jnf-emacs-packages/jnf-projectile.el
+++ jnf-emacs-packages/jnf-projectile.el
@@ -13,7 +13,7 @@
   :after pretty-hydra
   :diminish 'projectile-mode
   :config
-  (projectile-mode +1)
+  (projectile-mode 1)
   (defvar jnf/projectile--title (with-octicon "file-directory" "Projectile" 1 -0.05))
   (pretty-hydra-define jnf/projectile--menu (:foreign-keys warn :title jnf/projectile--title :quit-key "q" :exit t)
     ("Projectile" (
@@ -22,17 +22,18 @@
                    ("f" projectile-find-file-dwim   "Find File DWIM…")
                    ("i" projectile-ibuffer          "Ibuffer…")
                    ("k" projectile-kill-buffers     "Kill Buffers…")
-                   ("o" projectile-multi-occur      "Occurs in Project…")
+                   ("o" projectile-multi-occur      "Occurs in project…")
+                   ("p" projectile-switch-project   "Switch project…")
                    ("r" projectile-recentf          "Recent File Select…")
                    ("t" projectile-find-file        "Find File (aka CMD+t)…")
                    )))
   :custom
   (projectile-project-search-path '("~/git/"))
   :bind
-  (:map projectile-mode-map (("C-c p" . projectile-command-map)))
+  (:map projectile-mode-map (("C-c p" . jnf/projectile--menu/body)))
   :bind
   ("s-t" . projectile-find-file)
-  ("C-c p" . projectile-command-map)
+  ("C-c p" . jnf/projectile--menu/body)
   ("C-s-p". jnf/projectile--menu/body)
   ("s-." . projectile-toggle-between-implementation-and-test))
 
