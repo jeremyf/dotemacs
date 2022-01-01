022dcb201f72ffdca1fe6802d5b997d3ad92c842
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-12-31 21:31:18 -0500

    Updating org to use built-in

--- jnf-emacs-packages/jnf-org.el
+++ jnf-emacs-packages/jnf-org.el
@@ -18,13 +18,13 @@
   returning-list)
 
 (use-package org
-  :straight t
+  ;; :straight t
   ;; :straight (org
   ;;            :type git
   ;;            :url "https://git.savannah.gnu.org/git/emacs/org-mode.git"
   ;;            :commit "73875939a8b5545ac53a86ec467239f510d14de8" ;; 9.5 stable
   ;;            )
-  ;; :straight (org :type built-in)
+  :straight (org :type built-in)
   :config (setq
            org-directory "~/git/org"
            org-agenda-files (jnf/org-agenda-files
@@ -101,81 +101,71 @@
          ("C-c c" . org-capture)
          ("C-s-t" . org-toggle-link-display)))
 
-;; (use-package org-sidebar
-;;   :straight (org-sidebar :type git
-;;                          :host github
-;;                          :repo "alphapapa/org-sidebar"))
-
-;; Uncomment to always launch org mode with a sidebar tree
-;; (add-hook 'org-mode-hook #'org-sidebar-tree)
-
-
 ;; To make Org mode take care of versioning of attachments for you,
 ;; add the following to your Emacs config:
-;;
 (require 'org-attach-git)
 
-;; https://github.com/alphapapa/unpackaged.el#download-and-attach-remote-files
-;;;###autoload
-(defun unpackaged/org-attach-download (url)
-  "Download file at URL and attach with `org-attach'.
-Interactively, look for URL at point, in X clipboard, and in
-`kill-ring', prompting if not found.  With prefix, prompt for URL."
-  (interactive (list (if current-prefix-arg
-                         (read-string "URL: ")
-                       (or (org-element-property :raw-link (org-element-context))
-                           (org-web-tools--get-first-url)
-                           (read-string "URL: ")))))
-  (when (yes-or-no-p (concat "Attach file at URL: " url))
-    (let* ((temp-dir (make-temp-file "org-attach-download-" 'dir))
-           (basename (file-name-nondirectory (directory-file-name url)))
-           (local-path (expand-file-name basename temp-dir))
-           size)
-      (unwind-protect
-          (progn
-            (url-copy-file url local-path 'ok-if-exists 'keep-time)
-            (setq size (file-size-human-readable
-                        (file-attribute-size
-                         (file-attributes local-path))))
-            (org-attach-attach local-path nil 'mv)
-            (message "Attached %s (%s)" url size))
-        (delete-directory temp-dir)))))
+;; ;; https://github.com/alphapapa/unpackaged.el#download-and-attach-remote-files
+;; ;;;###autoload
+;; (defun unpackaged/org-attach-download (url)
+;;   "Download file at URL and attach with `org-attach'.
+;; Interactively, look for URL at point, in X clipboard, and in
+;; `kill-ring', prompting if not found.  With prefix, prompt for URL."
+;;   (interactive (list (if current-prefix-arg
+;;                          (read-string "URL: ")
+;;                        (or (org-element-property :raw-link (org-element-context))
+;;                            (org-web-tools--get-first-url)
+;;                            (read-string "URL: ")))))
+;;   (when (yes-or-no-p (concat "Attach file at URL: " url))
+;;     (let* ((temp-dir (make-temp-file "org-attach-download-" 'dir))
+;;            (basename (file-name-nondirectory (directory-file-name url)))
+;;            (local-path (expand-file-name basename temp-dir))
+;;            size)
+;;       (unwind-protect
+;;           (progn
+;;             (url-copy-file url local-path 'ok-if-exists 'keep-time)
+;;             (setq size (file-size-human-readable
+;;                         (file-attribute-size
+;;                          (file-attributes local-path))))
+;;             (org-attach-attach local-path nil 'mv)
+;;             (message "Attached %s (%s)" url size))
+;;         (delete-directory temp-dir)))))
 
-;; https://github.com/alphapapa/unpackaged.el#ensure-blank-lines-between-headings-and-before-contents
-;;;###autoload
-(defun unpackaged/org-fix-blank-lines (&optional prefix)
-  "Ensure blank lines exist between headings and content.
+;; ;; https://github.com/alphapapa/unpackaged.el#ensure-blank-lines-between-headings-and-before-contents
+;; ;;;###autoload
+;; (defun unpackaged/org-fix-blank-lines (&optional prefix)
+;;   "Ensure blank lines exist between headings and content.
 
-With PREFIX, operate on whole buffer.  Ensures that blank lines
-exist after each headings's drawers."
-  (interactive "P")
-  (org-map-entries (lambda ()
-                     (org-with-wide-buffer
-                      ;; `org-map-entries' narrows the buffer, which prevents us from seeing
-                      ;; newlines before the current heading, so we do this part widened.
-                      (while (not (looking-back "\n\n" nil))
-                        ;; Insert blank lines before heading.
-                        (insert "\n")))
-                     (let ((end (org-entry-end-position)))
-                       ;; Insert blank lines before entry content
-                       (forward-line)
-                       (while (and (org-at-planning-p)
-                                   (< (point) (point-max)))
-                         ;; Skip planning lines
-                         (forward-line))
-                       (while (re-search-forward org-drawer-regexp end t)
-                         ;; Skip drawers. You might think that `org-at-drawer-p' would suffice, but
-                         ;; for some reason it doesn't work correctly when operating on hidden text.
-                         ;; This works, taken from `org-agenda-get-some-entry-text'.
-                         (re-search-forward "^[ \t]*:END:.*\n?" end t)
-                         (goto-char (match-end 0)))
-                       (unless (or (= (point) (point-max))
-                                   (org-at-heading-p)
-                                   (looking-at-p "\n"))
-                         (insert "\n"))))
-                   t (if prefix
-                         nil
-                       'tree)))
+;; With PREFIX, operate on whole buffer.  Ensures that blank lines
+;; exist after each headings's drawers."
+;;   (interactive "P")
+;;   (org-map-entries (lambda ()
+;;                      (org-with-wide-buffer
+;;                       ;; `org-map-entries' narrows the buffer, which prevents us from seeing
+;;                       ;; newlines before the current heading, so we do this part widened.
+;;                       (while (not (looking-back "\n\n" nil))
+;;                         ;; Insert blank lines before heading.
+;;                         (insert "\n")))
+;;                      (let ((end (org-entry-end-position)))
+;;                        ;; Insert blank lines before entry content
+;;                        (forward-line)
+;;                        (while (and (org-at-planning-p)
+;;                                    (< (point) (point-max)))
+;;                          ;; Skip planning lines
+;;                          (forward-line))
+;;                        (while (re-search-forward org-drawer-regexp end t)
+;;                          ;; Skip drawers. You might think that `org-at-drawer-p' would suffice, but
+;;                          ;; for some reason it doesn't work correctly when operating on hidden text.
+;;                          ;; This works, taken from `org-agenda-get-some-entry-text'.
+;;                          (re-search-forward "^[ \t]*:END:.*\n?" end t)
+;;                          (goto-char (match-end 0)))
+;;                        (unless (or (= (point) (point-max))
+;;                                    (org-at-heading-p)
+;;                                    (looking-at-p "\n"))
+;;                          (insert "\n"))))
+;;                    t (if prefix
+;;                          nil
+;;                        'tree)))
 
 (use-package org-superstar
   :straight t
@@ -208,7 +198,7 @@ exist after each headings's drawers."
 ;; implementation of org-babel.
 (setq org-plantuml-jar-path (concat (string-trim (shell-command-to-string "brew-path plantuml")) "/libexec/plantuml.jar"))
 
-;; Insert immediate timestamp at point.
+;; ;; Insert immediate timestamp at point.
 (defun jnf/org-insert-immediate-active-timestamp ()
   "Insert an active date for today.  If given the universal arg (e.g., C-u) insert a timestamp instead."
   (interactive)
@@ -219,7 +209,7 @@ exist after each headings's drawers."
 (global-set-key (kbd "<f2>") 'jnf/org-insert-immediate-active-timestamp)
 (global-set-key (kbd "s-2") 'jnf/org-insert-immediate-active-timestamp)
 
-;; https://kitchingroup.cheme.cmu.edu/blog/2016/06/16/Copy-formatted-org-mode-text-from-Emacs-to-other-applications/
+;; ;; https://kitchingroup.cheme.cmu.edu/blog/2016/06/16/Copy-formatted-org-mode-text-from-Emacs-to-other-applications/
 (defun jnf/formatted-copy-org-to-html ()
   "Export region to HTML, and copy it to the clipboard."
   (interactive)
@@ -233,22 +223,6 @@ exist after each headings's drawers."
          "textutil -stdin -format html -convert rtf -stdout | pbcopy"))
       (kill-buffer buf))))
 (global-set-key (kbd "C-M-s-c") 'jnf/formatted-copy-org-to-html)
-
-;; For some reason, when I load emacs in daemon mode, the daemon
-;; process is the process now renders the GET prompts for the
-;; mini-buffer.  When I load the file interactively, I don't
-;; experience the same problem.  So, until this resolves, I'll need to
-;; load roam via an interactive command.
-;; (global-set-key (kbd "<f10>") `(lambda ()
-;;                                 (interactive)
-;;                                 (require 'jnf-org-roam.el)
-;;                                 ))
-
-;; A package to assist in handling annotations; I wish that DocView
-;; better rendered the PDF.  It's a little too pixelated.
-
-;; (setq org-latex-pdf-process
-;;       '("latexmk -pdflatex='pdflatex -interaction nonstopmode' -pdf -bibtex -f %f"))
 
 (eval-after-load 'ox '(require 'ox-koma-letter))
 
