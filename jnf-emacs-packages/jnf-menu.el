a656b7ff49c9af662421035f700e86d8f78e267c
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2022-01-27 20:57:21 -0500

    Tidying up transient declarations

--- jnf-emacs-packages/jnf-menu.el
+++ jnf-emacs-packages/jnf-menu.el
@@ -32,16 +32,15 @@ Add hook to each HOOKS provided."
 
 (minor-mode-maker :title "Burning Wheel Gold" :abbr "bwg" :hooks (list 'org-mode-hook 'markdown-mode-hook))
 
-(defun burning-wheel-gold-menu-items ()
+(defun rpg-menu-items ()
   "Return a `transient' compliant list for BWG items."
   (list
-   ["BWG References"
-    :if-non-nil jnf-bwg-minor-mode
-    ("b c" "Circles" jnf/qh--bwg-circles-obstacles)
-    ("b d" "Difficulty (Absolute)" jnf/qh--bwg-absolute-difficulty)
-    ("b e" "Exponent" jnf/qh--bwg-expertise-exponent)
-    ("b s" "Steel" jnf/qh--bwg-steel-test-adjustments)
-    ("b w" "Wises" jnf/qh--bwg-wises)]))
+   ["Game References"
+    ("b c" "Circles" jnf/qh--bwg-circles-obstacles :if-non-nil jnf-bwg-minor-mode)
+    ("b d" "Difficulty (Absolute)" jnf/qh--bwg-absolute-difficulty :if-non-nil jnf-bwg-minor-mode)
+    ("b e" "Exponent" jnf/qh--bwg-expertise-exponent :if-non-nil jnf-bwg-minor-mode)
+    ("b s" "Steel" jnf/qh--bwg-steel-test-adjustments :if-non-nil jnf-bwg-minor-mode)
+    ("b w" "Wises" jnf/qh--bwg-wises :if-non-nil jnf-bwg-minor-mode)]))
 
 (minor-mode-maker :title "Stars without Number" :abbr "swn")
 (minor-mode-maker :title "Worlds without Number" :abbr "wwn")
@@ -68,14 +67,15 @@ Add hook to each HOOKS provided."
 (define-transient-command jnf/menu-dwim ()
   "A \"super\" menu of context specific functions."
   ["Take on Rules"
-   ["Wrapping"    ("w a" "[A] link at point or region…" jnf/tor-wrap-link-active-region-dwim  :if-derived html-mode)
-    ("w c" "[C]ite point or region…" jnf/tor-wrap-cite-active-region-dwim  :if-derived html-mode)
-    ("w d" "[D]ate point or region…" jnf/tor-wrap-date  :if-derived html-mode)
-    ("w f" "Wrap word or region in pseudo-d[f]n…" jnf/tor-wrap-as-pseudo-dfn  :if-derived html-mode)
-    ("w m" "[M]argin-note line or region…" jnf/tor-wrap-as-marginnote-dwim  :if-derived html-mode)
-    ("w p" "Wrap point or region as [P]oem…" jnf/tor-wrap-in-poem  :if-derived html-mode)
-    ("w s" "[S]ide-note sentence or region…" jnf/tor-wrap-as-sidenote-dwim  :if-derived html-mode)
-    ("w w" "[W]rap point or region in html…" jnf/tor-wrap-in-html-tag  :if-derived html-mode)
+   ["Wrapping"
+    ("w a" "[A] link at point or region…" jnf/tor-wrap-link-active-region-dwim  :if-derived (or markdown-mode html-mode))
+    ("w c" "[C]ite point or region…" jnf/tor-wrap-cite-active-region-dwim  :if-derived (or markdown-mode html-mode))
+    ("w d" "[D]ate point or region…" jnf/tor-wrap-date  :if-derived (or markdown-mode html-mode))
+    ("w f" "Wrap word or region in pseudo-d[f]n…" jnf/tor-wrap-as-pseudo-dfn  :if-derived (or markdown-mode html-mode))
+    ("w m" "[M]argin-note line or region…" jnf/tor-wrap-as-marginnote-dwim  :if-derived (or markdown-mode html-mode))
+    ("w p" "Wrap point or region as [P]oem…" jnf/tor-wrap-in-poem  :if-derived (or markdown-mode html-mode))
+    ("w s" "[S]ide-note sentence or region…" jnf/tor-wrap-as-sidenote-dwim  :if-derived (or markdown-mode html-mode))
+    ("w w" "[W]rap point or region in html…" jnf/tor-wrap-in-html-tag  :if-derived (or markdown-mode html-mode))
     ]
    ["Posts"
     :if-non-nil jnf-tor-minor-mode
@@ -93,17 +93,17 @@ Add hook to each HOOKS provided."
     ("c c" "Create [c]hange log entry…" jnf/tor-find-changelog-and-insert-entry)
     ("c p" "Create [p]ost…" jnf/tor-create-post)
     ("c s" "Create [s]eries…" jnf/tor-find-series-and-insert-entry)
-    ("k h" "Kill slug version of given [h]eading…" jnf/kill-new-markdown-heading-as-slug :if-derived markdown-mode)
+    ("k h" "Kill slug version of given [h]eading…" jnf/kill-new-markdown-heading-as-slug :if-derived (or markdown-mode html-mode))
     ("t h m" "Toggle hammerspoon editor mode" hammerspoon-toggle-mode :if-non-nil hammerspoon-edit-minor-mode)
     ("t p" "Tidy pull request" jnf/forem-tidy-pull-request :if-non-nil hammerspoon-edit-minor-mode)
     ]])
 
 (transient-append-suffix 'jnf/menu-dwim (list 0)
   `[,@(minor-modes-menu-items)
-    ,@(burning-wheel-gold-menu-items)])
+    ,@(rpg-menu-items)])
 
 (transient-insert-suffix 'org-menu (list 0)
   `[,@(minor-modes-menu-items)
-    ,@(burning-wheel-gold-menu-items)])
+    ,@(rpg-menu-items)])
 
 (global-set-key (kbd "C-c m") 'jnf/menu-dwim)
