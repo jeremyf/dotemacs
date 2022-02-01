c464124023fa200cf41517ac8c1e5020720069de
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2022-01-31 23:41:10 -0500

    Tidying up cape usage

--- jnf-emacs-packages/jnf-corfu.el
+++ jnf-emacs-packages/jnf-corfu.el
@@ -50,37 +50,9 @@
          ("C-M-/" . dabbrev-expand)))
 
 
-;; (use-package cape
-;;   :straight t
-;;   ;; Bind dedicated completion commands
-;;   :bind (("C-c p p" . completion-at-point) ;; capf
-;;          ("C-c p t" . complete-tag)        ;; etags
-;;          ("C-c p d" . cape-dabbrev)        ;; or dabbrev-completion
-;;          ("C-c p f" . cape-file)
-;;          ("C-c p k" . cape-keyword)
-;;          ("C-c p s" . cape-symbol)
-;;          ("C-c p a" . cape-abbrev)
-;;          ("C-c p i" . cape-ispell)
-;;          ("C-c p l" . cape-line)
-;;          ("C-c p w" . cape-dict)
-;;          ;; ("C-c p \\" . cape-tex)
-;;          ("C-c p &" . cape-sgml)
-;;          ;; ("C-c p r" . cape-rfc1345)
-;; 	 )
-;;   :init
-;;   ;; Add `completion-at-point-functions', used by `completion-at-point'.
-;;   (add-to-list 'completion-at-point-functions #'cape-file)
-;;   ;; (add-to-list 'completion-at-point-functions #'cape-tex)
-;;   (add-to-list 'completion-at-point-functions #'cape-dabbrev)
-;;   (add-to-list 'completion-at-point-functions #'cape-keyword)
-;;   ;;(add-to-list 'completion-at-point-functions #'cape-sgml)
-;;   ;;(add-to-list 'completion-at-point-functions #'cape-rfc1345)
-;;   ;;(add-to-list 'completion-at-point-functions #'cape-abbrev)
-;;   (add-to-list 'completion-at-point-functions #'cape-ispell)
-;;   (add-to-list 'completion-at-point-functions #'cape-dict)
-;;   ;;(add-to-list 'completion-at-point-functions #'cape-symbol)
-;;   ;;(add-to-list 'completion-at-point-functions #'cape-line)
-;;   )
+(use-package cape
+  :straight t
+  :bind (("C-c p" . completion-at-point)))
 
 (use-package emacs
   :straight nil
