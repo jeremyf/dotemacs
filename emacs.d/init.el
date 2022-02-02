bf7565011e5f927d31e96759cc866c7929840779
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2022-02-02 12:07:25 -0500

    Moving org to configuration

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -41,9 +41,9 @@
 ;; (load "jnf-corfu.el" nil jnf-silence-loading-log)
 
 ;; (load "jnf-in-buffer.el" nil jnf-silence-loading-log)
-(load "jnf-macros.el" nil jnf-silence-loading-log)
-(load "jnf-git.el" nil jnf-silence-loading-log)
-(load "jnf-org.el" nil jnf-silence-loading-log)
+;; (load "jnf-macros.el" nil jnf-silence-loading-log)
+;; (load "jnf-git.el" nil jnf-silence-loading-log)
+;; (load "jnf-org.el" nil jnf-silence-loading-log)
 (load "jnf-org-roam-v3.el" nil jnf-silence-loading-log)
 
 (load "jnf-spelling.el" nil jnf-silence-loading-log)
