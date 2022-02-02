327a5278183135c00ff9d83d7550a91a9aa19322
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2022-02-02 08:14:35 -0500

    Literate configuring marginalia, ripgrep, savehist

--- jnf-emacs-packages/jnf-consult.el
+++ jnf-emacs-packages/jnf-consult.el
@@ -5,33 +5,6 @@
 ;;; Code:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package ripgrep
-  :init (setq ripgrep-arguments "--ignore-case")
-  :straight t)
-
-;; A front-end for the fzf bindings.
-(use-package fzf
-  :straight t)
-
-(use-package savehist
-  :straight t
-  :init
-  (savehist-mode))
-
-;; Enable richer annotations using the Marginalia package
-(use-package marginalia
-  :straight t
-  ;; Either bind `marginalia-cycle` globally or only in the minibuffer
-  :bind (:map minibuffer-local-map
-         ("M-A" . marginalia-cycle))
-
-  ;; The :init configuration is always executed (Not lazy!)
-  :init
-
-  ;; Must be in the :init section of use-package such that the mode gets
-  ;; enabled right away. Note that this forces loading the package.
-  (marginalia-mode))
-
 ;; Example configuration for Consult
 ;; https://github.com/minad/consult
 (use-package consult
