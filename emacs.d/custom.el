2753841ad23aa92503a532937aaa751444ac8b70
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2022-02-01 22:30:41 -0500

    Extracting custom.el

---
+++ emacs.d/custom.el
@@ -0,0 +1,35 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#fdf0ed" "#e95678" "#29d398" "#fadad1" "#26bbd9" "#ee64ac" "#26bbd9" "#403c3d"])
+ '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
+ '(column-number-mode t)
+ '(company-show-quick-access t nil nil "Customized with use-package company")
+ '(custom-safe-themes
+   '("7afad8f4707c84129e4cb1e5bad4feb0a9f0db02e1cfadb029921a0bde693d1e" "fd9e60866accaa68c8bede88623478e74b7c6e7c91765b0cdf11ddf04a5ff803" "7a994c16aa550678846e82edc8c9d6a7d39cc6564baaaacc305a3fdc0bd8725f" "e1ef2d5b8091f4953fe17b4ca3dd143d476c106e221d92ded38614266cea3c8b" "8607fdf417935af22922d10b4664a4ead5a64c01b55ac9e4eb9f4da9d177f612" "250268d5c0b4877cc2b7c439687f8145a2c85a48981f7070a72c7f47a2d2dc13" "23ba4b4ba4d1c989784475fed58919225db8d9a9751b32aa8df835134fe7ba6f" default))
+ '(delete-selection-mode t)
+ '(dired-listing-switches "-laGhpX")
+ '(dired-use-ls-dired t)
+ '(flycheck-checkers
+   '(ada-gnat asciidoctor asciidoc awk-gawk bazel-buildifier c/c++-clang c/c++-gcc c/c++-cppcheck cfengine chef-foodcritic coffee coffee-coffeelint coq css-csslint css-stylelint cuda-nvcc cwl d-dmd dockerfile-hadolint elixir-credo emacs-lisp emacs-lisp-checkdoc ember-template erlang-rebar3 erlang eruby-erubis eruby-ruumba fortran-gfortran go-gofmt go-golint go-vet go-build go-test go-errcheck go-unconvert go-staticcheck groovy haml handlebars haskell-stack-ghc haskell-ghc haskell-hlint html-tidy javascript-eslint javascript-jshint javascript-standard json-jsonlint json-python-json json-jq jsonnet less less-stylelint llvm-llc lua-luacheck lua markdown-markdownlint-cli markdown-mdl nix nix-linter opam perl perl-perlcritic php php-phpmd php-phpcs processing proselint protobuf-protoc protobuf-prototool pug puppet-parser puppet-lint python-flake8 python-pylint python-pycompile python-mypy r-lintr racket rpm-rpmlint rst-sphinx rst ruby-standard ruby-rubylint ruby ruby-jruby rust-cargo rust rust-clippy scala scala-scalastyle scheme-chicken scss-lint scss-stylelint sass/scss-sass-lint sass scss sh-bash sh-posix-dash sh-posix-bash sh-zsh sh-shellcheck slim slim-lint sql-sqlint systemd-analyze tcl-nagelfar terraform terraform-tflint tex-chktex tex-lacheck texinfo textlint typescript-tslint verilog-verilator vhdl-ghdl xml-xmlstarlet xml-xmllint yaml-jsyaml yaml-ruby yaml-yamllint))
+ '(global-display-line-numbers-mode t)
+ '(org-export-backends '(ascii html icalendar latex md odt))
+ '(safe-local-variable-values
+   '((jnf-tor-minor-mode . 1)
+     (take-on-rules-minor-mode . 1)
+     (encoding . utf-8)))
+ '(show-paren-mode t)
+ '(typopunct-buffer-language 'english)
+ '(use-package-always-ensure t)
+ '(warning-suppress-log-types '((comp)))
+ '(warning-suppress-types '((comp))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -42,6 +42,9 @@
 ;; use-package syntax which is often how things are documented.
 (straight-use-package 'use-package)
 
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(load custom-file :noerror)
+
 (load "jnf-basic-config.el" nil jnf-silence-loading-log)
 (load "jnf-config.el" nil jnf-silence-loading-log)
 
@@ -53,35 +56,6 @@
 (load "jnf-projectile.el" nil jnf-silence-loading-log)
 (load "jnf-vertico.el" nil jnf-silence-loading-log)
 (load "jnf-consult.el" nil jnf-silence-loading-log)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#fdf0ed" "#e95678" "#29d398" "#fadad1" "#26bbd9" "#ee64ac" "#26bbd9" "#403c3d"])
- '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
- '(column-number-mode t)
- '(company-show-quick-access t nil nil "Customized with use-package company")
- '(custom-safe-themes
-   '("7afad8f4707c84129e4cb1e5bad4feb0a9f0db02e1cfadb029921a0bde693d1e" "fd9e60866accaa68c8bede88623478e74b7c6e7c91765b0cdf11ddf04a5ff803" "7a994c16aa550678846e82edc8c9d6a7d39cc6564baaaacc305a3fdc0bd8725f" "e1ef2d5b8091f4953fe17b4ca3dd143d476c106e221d92ded38614266cea3c8b" "8607fdf417935af22922d10b4664a4ead5a64c01b55ac9e4eb9f4da9d177f612" "250268d5c0b4877cc2b7c439687f8145a2c85a48981f7070a72c7f47a2d2dc13" "23ba4b4ba4d1c989784475fed58919225db8d9a9751b32aa8df835134fe7ba6f" default))
- '(delete-selection-mode t)
- '(dired-listing-switches "-laGhpX")
- '(dired-use-ls-dired t)
- '(flycheck-checkers
-   '(ada-gnat asciidoctor asciidoc awk-gawk bazel-buildifier c/c++-clang c/c++-gcc c/c++-cppcheck cfengine chef-foodcritic coffee coffee-coffeelint coq css-csslint css-stylelint cuda-nvcc cwl d-dmd dockerfile-hadolint elixir-credo emacs-lisp emacs-lisp-checkdoc ember-template erlang-rebar3 erlang eruby-erubis eruby-ruumba fortran-gfortran go-gofmt go-golint go-vet go-build go-test go-errcheck go-unconvert go-staticcheck groovy haml handlebars haskell-stack-ghc haskell-ghc haskell-hlint html-tidy javascript-eslint javascript-jshint javascript-standard json-jsonlint json-python-json json-jq jsonnet less less-stylelint llvm-llc lua-luacheck lua markdown-markdownlint-cli markdown-mdl nix nix-linter opam perl perl-perlcritic php php-phpmd php-phpcs processing proselint protobuf-protoc protobuf-prototool pug puppet-parser puppet-lint python-flake8 python-pylint python-pycompile python-mypy r-lintr racket rpm-rpmlint rst-sphinx rst ruby-standard ruby-rubylint ruby ruby-jruby rust-cargo rust rust-clippy scala scala-scalastyle scheme-chicken scss-lint scss-stylelint sass/scss-sass-lint sass scss sh-bash sh-posix-dash sh-posix-bash sh-zsh sh-shellcheck slim slim-lint sql-sqlint systemd-analyze tcl-nagelfar terraform terraform-tflint tex-chktex tex-lacheck texinfo textlint typescript-tslint verilog-verilator vhdl-ghdl xml-xmlstarlet xml-xmllint yaml-jsyaml yaml-ruby yaml-yamllint))
- '(global-display-line-numbers-mode t)
- '(org-export-backends '(ascii html icalendar latex md odt))
- '(safe-local-variable-values
-   '((jnf-tor-minor-mode . 1)
-     (take-on-rules-minor-mode . 1)
-     (encoding . utf-8)))
- '(show-paren-mode t)
- '(typopunct-buffer-language 'english)
- '(use-package-always-ensure t)
- '(warning-suppress-log-types '((comp)))
- '(warning-suppress-types '((comp))))
 
 (load "jnf-epub.el" nil jnf-silence-loading-log)
 (load "jnf-modes.el" nil jnf-silence-loading-log)
@@ -103,14 +77,14 @@
 (load "jnf-enh-ruby.el" nil jnf-silence-loading-log)
 (load "jnf-lsp-mode.el" nil jnf-silence-loading-log)
 ;; (load "jnf-beancount.el" nil jnf-silence-loading-log)
-(load "jnf-tabs.el" nil jnf-silence-loading-log)
+;; (load "jnf-tabs.el" nil jnf-silence-loading-log)
 ;; (load "jnf-stars-without-number.el" nil jnf-silence-loading-log)
 (load "jnf-elfeed.el" nil jnf-silence-loading-log)
 ;; (load "jnf-fennel.el" nil jnf-silence-loading-log)
 (load "jnf-dired.el" nil jnf-silence-loading-log)
 (load "jnf-dice.el" nil jnf-silence-loading-log)
 (load "jnf-forem.el" nil jnf-silence-loading-log)
-(load "jnf-titlecase.el" nil jnf-silence-loading-log)
+;; (load "jnf-titlecase.el" nil jnf-silence-loading-log)
 (load "jnf-blogging.el" nil jnf-silence-loading-log)
 (load "jnf-help.el" nil jnf-silence-loading-log)
 (load "jnf-menu.el" nil jnf-silence-loading-log)
@@ -120,9 +94,3 @@
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
