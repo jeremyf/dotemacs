c5f75c0ff6a9e25182f6cb460cec95083c4f9c11
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2022-02-01 22:32:39 -0500

    Heading down the path of literate emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -53,7 +53,13 @@
 
 (load "jnf-display.el" nil jnf-silence-loading-log)
 
-(load "jnf-projectile.el" nil jnf-silence-loading-log)
+;; During loading of init file, disable checking filenames against the list of
+;; filetype handlers. This speeds up startup, as otherwise this list would be
+;; checked for every loaded .el and .elc file.
+(let ((file-name-handler-alist nil))
+  ;; Load the remainder of the configuration from the Org configuration file.
+  (org-babel-load-file (concat user-emacs-directory "configuration.org")))
+
 (load "jnf-vertico.el" nil jnf-silence-loading-log)
 (load "jnf-consult.el" nil jnf-silence-loading-log)
 
