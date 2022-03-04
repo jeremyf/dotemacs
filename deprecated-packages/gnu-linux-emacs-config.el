92e39abb6742470a0959ae6528c66aab641df48a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2022-03-04 18:22:53 -0500

    Removing deprecated packages

--- deprecated-packages/gnu-linux-emacs-config.el
+++
@@ -1,7 +0,0 @@
-;;; Commentary:
-;;
-;;  This package loads gnu-linux specific packages; It assumes that both
-;;  "use-package" and "straight-use-package" are loaded.
-;;
-;;; Code:
-(set-frame-font "JetBrains Mono 14" nil t)
