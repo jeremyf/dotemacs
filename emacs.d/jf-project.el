126504c4ad34afcd31f586193ea52f17337a2149
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2022-12-16 21:29:22 -0500

    Favoring single function for project jumping

--- emacs.d/jf-project.el
+++ emacs.d/jf-project.el
@@ -18,7 +18,7 @@
 ;;
 ;; I want a quick way navigate to those various workspaces.  I wrote
 ;; `jf/project/navigation-menu'.  I can set a current project via
-;; `jf/project/current-project-set'.  With that set, I can quickly jump to the
+;; `jf/project/current-project/set'.  With that set, I can quickly jump to the
 ;; aforementioned project workspaces.
 ;;
 ;; If one is not set, I use `jf/project/find-dwim' to attempt to determine
@@ -91,9 +91,10 @@
 	  (end-of-buffer)
 	  (error "No \"%s\" timesheet entry for today" project))))))
 
-(cl-defun jf/project/open-project-path-for (project)
+(bind-key "s-2" 'jf/project/open-project-path)
+(cl-defun jf/project/open-project-path (project)
   "Prompt for PROJECT then workspace and open that workspace."
-  (interactive (list (completing-read "Project: " (jf/project/list-projects))))
+  (interactive (list (jf/project/find-dwim)))
   (let*
       ;; Get the project's file name
       ((filename (cdar (jf/project/list-projects :project project)))
@@ -121,39 +122,6 @@ Determine the PROJECT by querying `jf/project/list-projects'."
   (let* ((project (or (s-presence project) (jf/project/find-dwim)))
 	 (filename (cdar (jf/project/list-projects :project project))))
     (find-file filename)))
-
-(defmacro jf/project/create-jump-to-fn (key)
-  (let* ((default_keyword (concat "PROJECT_PATH_TO_" (upcase key)))
-	 (key (downcase key))
-	 (fn (intern (concat "jf/project/jump-to-" key)))
-	 (docstring (concat "Jump to the given PROJECT's " key ".\n\n
-Check URL, directory, and filename.")))
-    `(cl-defun ,fn (&key project (keyword ,default_keyword))
-       ,docstring
-       (interactive)
-       (let* ((project (or (s-presence project) (jf/project/find-dwim)))
-	      (filename (cdar (jf/project/list-projects :project project))))
-	 (with-current-buffer (find-file-noselect filename)
-	   (if-let ((path (cadar (org-collect-keywords (list keyword)))))
-	       (cond
-		((s-starts-with? "http" path)
-		 (eww-browse-with-external-browser path))
-		((f-dir-p path)
-		 (dired path))
-		((f-file-p path)
-		 (find-file path))
-		(t (progn
-		     (message "WARNING: Project %s missing %s \"%s\""
-			      project ,key path)
-		     (jf/project/jump-to-notes :project project))))
-	     (progn
-	       (message "WARNING: Project %s has no %s" project ,key)
-	       (jf/project/jump-to-notes :project project))))))))
-
-(jf/project/create-jump-to-fn "board")
-(jf/project/create-jump-to-fn "code")
-(jf/project/create-jump-to-fn "remote")
-(jf/project/create-jump-to-fn "discussion")
 
 ;;;; Support Functions
 (cl-defun jf/project/list-projects (&key (project ".+")
@@ -253,63 +221,6 @@ When the `current-prefix-arg' is set always prompt for the project."
 	    (jf/project/get-project-from-current-clock)
 	    (jf/project/get-project-from-project-current)))
    (completing-read "Project: " (jf/project/list-projects))))
-
-;;;; Dispatch Menu
-
-;; I want a single command that will popup a menu.  Similar to my old org-roam
-;; context menus, I want to set the current project.
-;;
-;; I want the lower case keys to be for the current project and the upper case
-;; keys to prompt for project.
-
-(defvar jf/project/current-project
-  nil
-  "The current contextual project.")
-
-(bind-key "s-2" 'jf/project/navigation-menu)
-(transient-define-prefix jf/project/navigation-menu ()
-  "My Project menu."
-  ["Projects"
-   ["Current project"
-    ("." jf/project/current-project-set :transient t)
-    ("b" "Board…" (lambda () (interactive)
-		    (jf/project/jump-to-board
-		     :project jf/project/current-project)))
-    ("c" "Code…" (lambda () (interactive)
-		   (jf/project/jump-to-code
-		    :project jf/project/current-project)))
-    ("d" "Discussion…" (lambda () (interactive)
-			 (jf/project/jump-to-discussion
-			  :project jf/project/current-project)))
-    ("n" "Notes…" (lambda () (interactive)
-		    (jf/project/jump-to-notes
-		     :project jf/project/current-project)))
-    ("r" "Remote…" (lambda () (interactive)
-		     (jf/project/jump-to-remote
-		      :project jf/project/current-project)))
-    ("t" "Timesheet…" (lambda () (interactive)
-		     (jf/project/jump-to-timesheet
-		      :project jf/project/current-project)))]
-   ["Other projects"
-    ""
-    ("B" "Board…" jf/project/jump-to-board)
-    ("C" "Code…" jf/project/jump-to-code)
-    ("D" "Discussion…" jf/project/jump-to-discussion)
-    ("N" "Notes…" jf/project/jump-to-notes)
-    ("R" "Remote…" jf/project/jump-to-remote)
-    ("T" "Timesheet…" jf/project/jump-to-timesheet)]])
-
-(transient-define-suffix jf/project/current-project-set (project)
-  "Select FILES to use as source for work desk."
-  :description '(lambda ()
-		  (concat
-		   "Current: "
-		   (propertize
-		    (format "%s" jf/project/current-project)
-		    'face 'transient-argument)))
-  (interactive (list (completing-read "Project: " (jf/project/list-projects))))
-  ;; Ensure that the empty
-  (setq jf/project/current-project (s-presence project)))
 
 (provide 'jf-project)
 ;;; jf-project.el ends here
