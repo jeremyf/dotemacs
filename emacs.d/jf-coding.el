ab41d54958ddd32e8a9f30384dc7c0c356c3b227
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-09-08 21:57:34 -0400

    🎁 Adding debugger sniffer logic

--- emacs.d/jf-coding.el
+++ emacs.d/jf-coding.el
@@ -373,6 +373,22 @@ method, get the containing class."
   :mode (("\\.puml\\'" . plantuml-mode))
   :straight t)
 
+(defun jf/require-debugger ()
+  "Determine the correct debugger based on the Gemfile."
+  (let ((gemfile-lock (f-join (projectile-project-root) "Gemfile.lock")))
+    (if-let* ((f-exists? gemfile-lock)
+               (debuggers (s-split "\n"
+                            (shell-command-to-string
+                              (concat "rg \"^ +(byebug|debugger|pry-byebug|debug) \" "
+                                gemfile-lock " -r '$1' --only-matching | uniq")))))
+      (cond
+        ((member "byebug") "require 'byebug'; byebug")
+        ((member "debug") "require 'debug'; binding.break")
+        ((member "debugger") "require 'debugger'; debugger")
+        ((member "pry-byebug") "require 'pry-byebug'; binding.pry")
+        (t "require 'debug'; binding.break"))
+      "require 'debug'; binding.break")))
+
 (use-package rspec-mode
   ;; I write most of my Ruby tests using rspec.  This tool helps manage that
   ;; process.
