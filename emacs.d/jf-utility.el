26afcb9deeaeba3ed0a1a238363970a0e9c15e56
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-11-09 09:24:21 -0500

    Add ignore dirs for color-rg

--- emacs.d/jf-utility.el
+++ emacs.d/jf-utility.el
@@ -133,6 +133,7 @@
   ;; replacement.  But I'll keep both of them around.
   :straight t
   :commands (color-rg-read-file-type color-rg-search-input color-rg-read-input)
+  :custom (color-rg-search-ignore-rules "-g '!node_modules' -g '!dist' -g '!coverage' -g '!doc'")
   :config
   (defun jf/color-rg-search-project (prefix &optional keyword directory globs)
     "Dispatch to `color-rg-search-input' based on given PREFIX.
