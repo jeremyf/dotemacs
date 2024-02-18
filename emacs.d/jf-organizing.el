28d19b6823ad562903a793363f995ab15eafd196
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-02-18 08:59:10 -0500

    Adding ability to rebuild projectile cache

--- emacs.d/jf-organizing.el
+++ emacs.d/jf-organizing.el
@@ -18,11 +18,19 @@
 (use-package projectile
   ;; Convenient organization and commands for projects.
   :straight t
-  :config (projectile-mode 1)
   :custom (projectile-project-search-path '("~/git/"))
   ;; (projectile-git-fd-args "-H -0 -E hyrax-webapp -E .git -tf --strip-cwd-prefix -c never")
   ;; (projectile-git-submodule-command "")
-  :bind ("s-." . projectile-toggle-between-implementation-and-test))
+  :bind ("s-." . projectile-toggle-between-implementation-and-test)
+  :config
+  (projectile-mode 1)
+  (defun jf/projectile-reset-known-projects ()
+    "Reset known projects to `projectile-project-search-path'."
+    (interactive)
+    (require 's)
+    (dolist (proj projectile-known-projects)
+      (unless (s-starts-with? projectile-project-search-path proj)
+        (projectile-remove-known-project proj)))))
 
 (provide 'jf-organizing)
 ;;; jf-organizing.el ends here
