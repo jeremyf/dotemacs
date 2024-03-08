0eb0907d9a708ce5be7bdb898975173fd5e651fb
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-03-08 17:14:40 -0500

    ♻️ Working on custom modeline

--- emacs.d/jf-modeline.el
+++ emacs.d/jf-modeline.el
@@ -1,12 +1,12 @@
 (defvar-default mode-line-format '("%e" mode-line-front-space
-                                  (:propertize
-                                    ("" mode-line-mule-info mode-line-client mode-line-modified mode-line-remote)
-                                    display
-                                    (min-width
-                                      (5.0)))
-                                  mode-line-frame-identification mode-line-buffer-identification "   " mode-line-position
-                                  (vc-mode vc-mode)
-                                  "  " minions-mode-line-modes mode-line-misc-info mode-line-end-spaces))
+                                    (:propertize
+                                      ("" mode-line-mule-info mode-line-client mode-line-modified mode-line-remote)
+                                      display
+                                      (min-width
+                                        (5.0)))
+                                    mode-line-frame-identification mode-line-buffer-identification "   " mode-line-position
+                                    (vc-mode vc-mode)
+                                    "  " minions-mode-line-modes mode-line-misc-info mode-line-end-spaces))
 
 (defvar-local jf/mode-line-format/kbd-macro
   '(:eval
@@ -29,8 +29,8 @@
 (defvar-local jf/mode-line-format/narrow
   '(:eval
      (when (and (mode-line-window-selected-p)
-                 (buffer-narrowed-p)
-                 (not (derived-mode-p 'Info-mode 'help-mode 'special-mode 'message-mode)))
+             (buffer-narrowed-p)
+             (not (derived-mode-p 'Info-mode 'help-mode 'special-mode 'message-mode)))
        (propertize " Narrow " 'face 'mode-line-highlight))))
 
 (defvar-local jf/mode-line-format/misc-info
@@ -48,15 +48,12 @@
        '(eglot--managed-mode eglot--mode-line-format))))
 
 (defvar-local jf/mode-line-format/vc-branch
-    '(:eval
-      (when-let* (((mode-line-window-selected-p))
+  '(:eval
+     (when-let* (((mode-line-window-selected-p))
                   (file (buffer-file-name))
                   (backend (vc-backend file))
-                  ;; ((vc-git-registered file))
-                  (branch (jf/mode-line-format/vc-branch-name file backend))
-                   ;; (face (prot-modeline--vc-face file backend))
-                   )
-        (jf/mode-line-format/vc-details file branch))))
+                  (branch (jf/mode-line-format/vc-branch-name file backend)))
+       (jf/mode-line-format/vc-details file branch))))
 
 (defun jf/mode-line-format/vc-details (file branch)
   (concat
@@ -67,8 +64,8 @@
 (defun jf/mode-line-format/vc-branch-name (file backend)
   "Return VC branch name for FILE with BACKEND."
   (when-let ((rev (vc-working-revision file backend))
-             (branch (or (vc-git--symbolic-ref file)
-                         (substring rev 0 7))))
+              (branch (or (vc-git--symbolic-ref file)
+                        (substring rev 0 7))))
     branch))
 
 (dolist (construct '(
@@ -79,18 +76,20 @@
                       jf/mode-line-format/narrow
                       jf/mode-line-format/read-only-status
                       jf/mode-line-format/vc-branch
+                      mode-line-end-spaces
                       ))
   (put construct 'risky-local-variable t))
 
-(defvar-local mode-line-format '("%e"
-                                  " "
-                                  jf/mode-line-format/kbd-macro
-                                  jf/mode-line-format/narrow
-                                  jf/mode-line-format/read-only-status
+(setq-local mode-line-format '("%e"
+                                " "
+                                jf/mode-line-format/kbd-macro
+                                jf/mode-line-format/narrow
+                                jf/mode-line-format/read-only-status
 
-                                  jf/mode-line-format/major-mode-name
-                                  flymake-mode-line-format
-                                  jf/mode-line-format/vc-branch
-                                  jf/mode-line-format/eglot
-                                  jf/mode-line-format/misc-info
-                                  ))
+                                jf/mode-line-format/major-mode-name
+                                flymake-mode-line-format
+                                jf/mode-line-format/vc-branch
+                                jf/mode-line-format/eglot
+                                "   "
+                                jf/mode-line-format/misc-info
+                                ))
