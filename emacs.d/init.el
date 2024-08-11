3becaac527751e94e77afb79e2ff9b195fb17bf0
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-08-11 08:40:54 -0400

    feat(): add export elfeed link functionality

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4720,6 +4720,31 @@ PARG is for a conformant method signature."
     :face #'jf/org-faces-epigraph
     :follow #'denote-link-ol-follow)
 
+  (org-link-set-parameters "elfeed"
+    :follow #'elfeed-link-open
+    :store #'elfeed-link-store-link
+    :export #'elfeed-link-export-link)
+
+  (defun elfeed-link-export-link (link desc format _protocol)
+    "Export `org-mode' `elfeed' LINK with DESC for FORMAT."
+    (if (string-match "\\([^#]+\\)#\\(.+\\)" link)
+      (if-let* ((entry
+                  (elfeed-db-get-entry
+                    (cons (match-string 1 link)
+                      (match-string 2 link))))
+                 (url
+                   (elfeed-entry-link entry))
+                 (title
+                   (elfeed-entry-title entry)))
+        (pcase format
+          ('html (format "<a href=\"%s\">%s</a>" url desc))
+          ('md (format "[%s](%s)" desc url))
+          ('latex (format "\\href{%s}{%s}" url desc))
+          ('texinfo (format "@uref{%s,%s}" url desc))
+          (_ (format "%s (%s)" desc url)))
+        (format "%s (%s)" desc url))
+      (format "%s (%s)" desc link)))
+
   (defface jf/org-faces-date '((default :inherit link))
     "Face used to style `org-mode' date links in the buffer."
     :group 'denote-faces
