cdbd23ed76c9b07b3a63919bda9eed9c891de706
Jeremy Friesen <github@jeremyfriesen.com>
2025-05-08 13:21:27 -0400

    refactor(): add narrow-or-widen-dwim

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4073,6 +4073,37 @@ LaTeX package."
                (format-time-string
                  "%Y-%m-%d"))))
 
+(defun narrow-or-widen-dwim (p)
+  "Widen if buffer is narrowed, narrow-dwim otherwise.
+Dwim means: region, org-src-block, org-subtree, or
+defun, whichever applies first. Narrowing to
+org-src-block actually calls `org-edit-src-code'.
+
+With prefix P, don't widen, just narrow even if buffer
+is already narrowed."
+  ;; [Emacs narrow-or-widen-dwim · Endless Parentheses](https://endlessparentheses.com/emacs-narrow-or-widen-dwim.html)
+  (interactive "P")
+  (declare (interactive-only))
+  (cond ((and (buffer-narrowed-p) (not p)) (widen))
+        ((region-active-p)
+         (narrow-to-region (region-beginning)
+                           (region-end)))
+        ((derived-mode-p 'org-mode)
+         ;; `org-edit-src-code' is not a real narrowing
+         ;; command. Remove this first conditional if
+         ;; you don't want it.
+         (cond ((ignore-errors (org-edit-src-code) t)
+                (delete-other-windows))
+               ((ignore-errors (org-narrow-to-block) t))
+               (t (org-narrow-to-subtree))))
+        ((derived-mode-p 'latex-mode)
+         (LaTeX-narrow-to-environment))
+        (t (narrow-to-defun))))
+
+;; This line actually replaces Emacs' entire narrowing
+;; keymap, that's how much I like this command. Only
+;; copy it if that's what you want.
+(define-key ctl-x-map "n" #'narrow-or-widen-dwim)
 
 ;; Another task at end of month is to transcribing my agenda’s
 ;; timesheet to entries in our time tracking software.  From the day’s
