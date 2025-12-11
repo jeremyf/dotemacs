d0fb8510bb6582fcc633644888fe2d6a197cdf4f
Jeremy Friesen <github@jeremyfriesen.com>
2025-12-11 18:22:29 -0500

    feat(): initial work on mythic bastionland hex
    This is a functional work in progress.  And I'm going to work on a few
    more todo items, after I move things into a "namespace."

--- emacs.d/mythic-bastionland.el
+++ emacs.d/mythic-bastionland.el
@@ -17,7 +17,7 @@
 ;; - What, if any, Landmark is present?
 ;; - What is the nearest Myth?
 ;; - What is the direction towards a Myth?
-;; - Grab a myth that is not the closest.
+;; - Get a Myth that is not the closest.
 ;; - Moving from Hex A to Hex B, do I encounter a Barrier?
 ;;
 ;; And I realized that I could randomly generate a map, encode it in an
@@ -26,11 +26,31 @@
 ;;
 ;; I begin my research: https://www.redblobgames.com/grids/parts/; which
 ;; leads me to https://www.redblobgames.com/grids/hexagons/.
+;;
+;; For this implementation, I making hard and fast assumptions:
+;;
+;; - The map is 12 x 12.
+;; - The map orientation is jagged side on the left and right; which
+;;   means I'm using double height coordinates.
+;; - I'm not randomly placing holdings, instead relying on other maps.
+;; - Coordinates start at 0,0, which is the top-most left-most hex.
+;;
+;;; Future considerations
+;;
+;; - Replace the label at a given coordinate.  You'll need to specify
+;;   type and coordinate (or perhaps current string).
+;;
+;; Further I want to make it hard to read the map.
 (defun populate-map (holdings)
-  ;; Populate the initial land marks with the given holdings.
-  (let ((locations
-          (mapcar #'copy-list holdings))
-         (locations-to-place '())
+  "Populate the map with HOLDINGS.
+
+HOLDINGS is a list of `cons' with `car' as label and `cdr' as
+coordinates, as expressed in `prompt-for-coord'."
+  (let ((locations-to-place '())
+         ;; The already placed locations, preventing us from placing
+         ;; other features at those coordinates.
+         (locations
+          (mapcar (lambda (h) (cons (cdr h) (car h))) holdings))
          (myths '())
          (sanctums '())
          (dwellings '())
@@ -77,26 +97,61 @@
       (cons 'ruins ruins)
       (cons 'holdings holdings))))
 
+(defun mythic-bastionland-map-write ()
+  (f-write
+    (base64-encode-string
+    (format "%S" mythic-bastionland-map)
+    )
+    'utf-8-unix
+    "~/.local/bastionland.base64")
+  (shell-command "cd ~/.local; sha256sum bastionland.base64 > bastionland.checksum"))
+
+(defun mythic-bastionland-map ()
+  (or mythic-bastionland-map
+    (setq mythic-bastionland-map (mythic-bastionland-map-read))))
+
+(defun mythic-bastionland-map-read ()
+  (if (string= "bastionland.base64: OK\n"
+        (shell-command-to-string
+          "cd ~/.local; sha256sum -c bastionland.checksum"))
+    (read
+      (base64-decode-string
+        (f-read
+          "~/.local/bastionland.base64"
+          'utf-8-unix)
+      ))
+    (error "We have an unexpected bastionland.base64 file")))
+
+
+;; (setq mythic-bastionland-map
+;;   (populate-map '(("Castle" . (9 . 4)) ("Tower" . (0 . 1)) ("Castle" . (0 . 2)))))
+
 (defvar mythic-bastionland-map
   nil)
 
 (defun prompt-for-coord ()
+  "Prompt for Column and Row returning a `cons' cell.
+
+Where `car' is the column and `cdr' is the row."
   (cons
         (read-number "Column: ")
         (read-number "Row: ")))
 
 (defun hex-feature (coord)
-  "Echo the feature, if any, at (COL . ROW)."
+  "Echo the feature, if any, at the given COORD.
+
+See `prompt-for-coord'."
   (interactive
     (list
       (prompt-for-coord)))
   (message "%s"
     (or
-      (cdr (assoc coord (assoc 'locations mythic-bastionland-map)))
+      (cdr (assoc coord (assoc 'locations (mythic-bastionland-map))))
       "Nothing")))
 
 ;; https://www.redblobgames.com/grids/hexagons/#distances-doubled
 (defun hex-distance (to from)
+  "Both TO and FROM are `cons' as per `prompt-for-coord'."
   ;; We're assuming double height that is left/right side is saw-blade.
   (let ((dcol
           (abs (- (car to) (car from))))
@@ -104,15 +159,14 @@
            (abs (* 2 (- (cdr to) (cdr from))))))
     (+ dcol (max 0 (/ (- drow dcol) 2)))))
 
-(defun nearest-myth (from)
-  "Echo the nearest myth to (COL . ROW)."
+(defun nearest-myth (coord)
+  "Echo the nearest myth to COORD.
+
+See `prompt-for-coord'."
   (interactive
     (list (prompt-for-coord)))
   (let* ((distances
-            (seq-sort (lambda (l r) (< (car l) (car r)))
-              (mapcar (lambda (myth)
-                        (cons (hex-distance (cdr myth) from) (car myth)))
-                (cdr (assoc 'myths mythic-bastionland-map)))))
+           (myth-distances-from-closest-to-furthest coord))
           (shortest-distance
             (caar distances)))
     (message "%s"
@@ -122,14 +176,28 @@
                       (= shortest-distance (car cell)))
           distances))))))
 
+(defun myth-distances-from-closest-to-furthest (from)
+  "Sort distance FROM each myth into a list of `cons' cells.
+
+The `car' is the integer distance and the `cdr' is the name of the
+myth.`
+
+Note the FROM is conformant to the `prompt-for-coord'."
+  (seq-sort (lambda (l r) (< (car l) (car r)))
+    (mapcar (lambda (myth)
+              (cons (hex-distance (cdr myth) from) (car myth)))
+      (cdr (assoc 'myths (mythic-bastionland-map))))))
+
 (defun direction-to-myth (from myth)
-  "Calculate the direction from (COL . ROW) to MYTH"
+  "Calculate the direction FROM to MYTH.
+
+See `prompt-for-coord' for details of FROM."
   (interactive
     (list
       (prompt-for-coord)
-      (let ((myths (cdr (assoc 'myths mythic-bastionland-map))))
+      (let ((myths (cdr (assoc 'myths (mythic-bastionland-map)))))
         (assoc (completing-read "Myth: " myths nil t) myths))))
-  (message "%S"
+  (message "%s"
     (let* ((to (cdr myth)))
       (cond
         ((equal to from)
@@ -142,6 +210,7 @@
                           (float (- (* 2 (cdr to)) (* 2 (cdr from))))
                           (float (- (car to) (car from))))))
              (cond
+               ;; TODO: Determine which direction.
                ((or (>= slope 2) (<= slope -2))
                  "North/South")
                ((< 0.5 slope 2.0)
