ca0ef2ca6af90a1389c2fc5ee2eae8c1b3f691af
Jeremy Friesen <github@jeremyfriesen.com>
2025-12-12 23:03:19 -0500

    feat(mb): allow config for map generation

--- emacs.d/mythic-bastionland.el
+++ emacs.d/mythic-bastionland.el
@@ -88,83 +88,214 @@
 ;;            (left (11 . 5) right (10 . 6)) (left (9 . 1) right (10 . 0)))
 ;;  (holdings ("Tower" 9 . 3) ("Castle" 5 . 7) ("Fortress" 1 . 19) ("Town" 8 . 16)))
 
+;;; Generating Map with Example Config:
+
+;; (defalias 'mbc 'mythic-bastionland--col-row-to-coord)
+;; ;; Based on the presented game logic and reviewing the map, there are 6
+;; ;; places where the Mountain could be and still allow for the Beast to
+;; ;; also be along the shores of the lake.
+;; (let* (
+;;        ;; This is where Sir Weydlyn encountered Seer Tompot.
+;;        (sanctums
+;;         `(("Tompot (Tangled Seer)" . ,(mbc 8 1))))
+;;        ;; Based on Sir Weydlyn's encountered a "nearest myth" event,
+;;        ;; These are the coordinates in which no myth can occur.  The
+;;        ;; `car' (e.g. 1) means that The Beast myth is within one hex of
+;;        ;; the place where Sir Weydlyn encountered that first sign.
+;;        (myth-omits `((1 . (,(mbc 9 2) ,(mbc 10 1) ,(mbc 8 1)
+;;                            ,(mbc 10 2) ,(mbc 9 0) ,(mbc 8 2)))
+;;                      (2 . (,(mbc 8 0) ,(mbc 10 0) ,(mbc 11 0)
+;;                            ,(mbc 11 1) ,(mbc 11 2) ,(mbc 10 3)
+;;                            ,(mbc 9 3) ,(mbc 8 3) ,(mbc 7 2)
+;;                            ,(mbc 7 1) ,(mbc 7 0)))
+;;                      (3 . (,(mbc 6 0) ,(mbc 6 1) ,(mbc 6 2)
+;;                            ,(mbc 6 3) ,(mbc 7 3) ,(mbc 8 4)
+;;                            ,(mbc 9 4) ,(mbc 10 4) ,(mbc 11 3)))))
+;;        ;; a list of lists; each element's `car' is where the Mountain is
+;;        ;; and each elements `cdr' is a list of where the Beast could be,
+;;        ;; based on the two points of information.
+;;        (scenarios
+;;         `((,(mbc 10 4) . ((myth . (,(mbc 9 4) ,(mbc 8 5)))
+;;                           (omit . 2)))
+;;           (,(mbc 8 4) . ((myth . (,(mbc 9 5) ,(mbc 8 5)))
+;;                          (omit . 2)))
+;;           (,(mbc 9 2) . ((myth . (,(mbc 8 3) ,(mbc 8 4) ,(mbc 8 5)
+;;                                   ,(mbc 9 3) ,(mbc 9 4), (mbc 9 5)))
+;;                          (omit . nil)))
+;;           (,(mbc 9 3) . ((myth . (,(mbc 8 4) ,(mbc 8 5) ,(mbc 9 4)))
+;;                          (omit . 1)))
+;;           (,(mbc 9 4) .  ((myth . (,(mbc 8 4)))
+;;                           (omit . 2)))
+;;           (,(mbc 9 5) . ((myth . (,(mbc 8 5)))
+;;                          (omit . 3)))))
+;;        ;; Now pick a random scenario
+;;        (scenario
+;;         (seq-random-elt scenarios)))
+;;   (mythic-bastionland-map-generate
+;;    `(
+;;      ;; With the chosen random scenario, we assign the Moutain, then
+;;      ;; pick a random one for the Beast
+;;      (myths .
+;;       (("The Mountain" . ,(car scenario))
+;;        ("The Beast" . ,(seq-random-elt
+;;                         (alist-get 'myth (cdr scenario))))))
+;;      ;; The random map player facing map I started with, had these
+;;      ;; locations.
+;;      (holdings .
+;;       (("Tower" . (9 . 3)) ("Castle" . (5 . 7))
+;;        ("Fortress" . (1 . 19)) ("Town" . (8 . 16))))
+;;      (omit . (
+;;             ;; Sir Wedylyn crossed between these two potential
+;;             ;; barriers.
+;;             (barriers .
+;;                       ((,(mbc 8 1) . ,(mbc 9 1))
+;;                        (,(mbc 9 1) . ,(mbc 8 2))))
+;;             ;; With myths chosen, now assign where myths cannot be.
+;;             (myths . ,(alist-get
+;;                        (alist-get 'omit (cdr scenario))
+;;                        myth-omits)))))))
+
+
+;;
 ;;; Code:
-(defun mythic-bastionland-map-generate (holdings)
-  "Populate the map with HOLDINGS.
 
-HOLDINGS is a list of `cons' with `car' as label and `cdr' as
-coordinates, as expressed in `mythic-bastionland--col-row-to-coord'."
-  (let* ((locations-to-place '())
-          ;; The already placed locations, preventing us from placing
-          ;; other features at those coordinates.
-          (locations
-            (mapcar (lambda (h) (cons (cdr h) (car h))) holdings))
-          (myths '())
-          (sanctums '())
-          (dwellings '())
-          (monuments '())
-          (hazards '())
-          (curses '())
-          (ruins '()))
-    (dotimes (i 6)
-      (add-to-list 'locations-to-place
-        (cons 'myths (format "Myth %d" (+ 1 i)))))
-    (dolist (cell '((sanctums . "Sanctum")
-                     (dwellings . "Dwelling")
-                     (monuments . "Monument")
-                     (hazards . "Hazard")
-                     (curses . "Curse")
-                     (ruins . "Ruin")))
-      (dotimes (i (+ 3 (random 2)))
-        (add-to-list 'locations-to-place
-          (cons (car cell) (format "%s %d" (cdr cell) (+ 1 i))))))
+(defvar mythic-bastionland-features
+  '((myths . (6)) (holdings . (4))
+     (sanctums . (3 4)) (monuments . (3 4)) (dwellings . (3 4))
+     (hazards . (3 4)) (curses . (3 4)) (ruins . (3 4)))
+  "Feature types that are labeled, and thus renameable.  We also encode
+how many of these, in total, are to be placed.")
 
-    ;; Here I make a decision on the placement algorithm.  Loop through
-    ;; each place, then generate a random coordinate, and test if its
-    ;; occupied.
+(defun mythic-bastionland-map-generate (config)
+  "Generate and store `mythic-bastionland-map' via CONFIG.
+
+See `mythic-bastionland-features' for some of the `car' values of
+CONFIG.  Another is `barriers' (which are unamed).  Another is `omit',
+itself an alist, with the same `car' values as those in CONFIG (except
+`omit').
+
+As of <2025-12-12 Fri> adding `barriers' is not handled.  It is a
+feature I'd like to implement."
+  (let ((barriers nil)
+         (locations nil)
+         (locations-to-place nil)
+         (the-map nil))
+
+    ;; First put the locations on the map...no effort is taken to avoid
+    ;; location collisions.  Also, queue up further locations to place.
+    (cl-loop for (feature . count) in mythic-bastionland-features do
+      (let ((feat-locations
+              (alist-get feature config)))
+        ;; When we are given locations for this feature type, add it
+        ;; to the placed list.
+        (when feat-locations
+          (cl-loop for (label . coord) in feat-locations do
+            (cl-pushnew (cons coord label) locations)))
+
+        ;; Next queue up placing the remainder of locations for the
+        ;; feature type (accounting for what was already given).
+        (dotimes (i (- (seq-random-elt count) (length feat-locations)))
+          (cl-pushnew (cons feature
+                        (format "%s %s" feature (+ i 1)))
+            locations-to-place))
+
+        (cl-pushnew (cons feature feat-locations) the-map)))
+
+    ;; Now that we have our task list of what all needs adding.
     ;;
-    ;; When occupied, get a new-label random coordinate and try again.
-    ;; When unoccupied, add to the list and move to the next feature.
-    (dolist (to-place locations-to-place)
-      (let ((keep-trying t))
+    ;; This involves avoiding collisions with other placed features as
+    ;; well as heading the guidance of an omit coordinates for the given
+    ;; feature.
+    (cl-loop for (feature . label) in locations-to-place do
+      (let ((keep-trying t)
+             (omitted-feature-coordinates
+               (alist-get feature (alist-get 'omit the-map))))
         (while keep-trying
           (let ((coord
                   (mythic-bastionland--col-row-to-coord
                     (random 12) (random 12))))
-            (unless (assoc coord locations)
-              (let ((label (cdr to-place)))
+            (when (not (or
+                         (assoc coord locations)
+                         (member coord omitted-feature-coordinates)
+                         ))
+              (progn
                 (setq keep-trying nil)
                 ;; For locations we favor storing the (coord . label)
-                (add-to-list 'locations
-                  (cons coord label))
-                ;; For the specific feature, we favor storing (label
-                ;; . coord)
-                ;; (message "%S" to-place)
-                (add-to-list (car to-place)
-                  (cons label coord))))))))
-    (list
-      (cons 'locations locations)
-      (cons 'myths myths)
-      (cons 'sanctums sanctums)
-      (cons 'dwellings dwellings)
-      (cons 'monuments monuments)
-      (cons 'hazards hazards)
-      (cons 'curses curses)
-      (cons 'ruins ruins)
-      (cons 'barriers (mythic-bastionland--generate-barriers))
-      (cons 'holdings holdings))))
+                (cl-pushnew (cons coord label) locations)
+                ;; TODO add to list
+                (cl-pushnew (cons label coord)
+                  (alist-get feature the-map))))))))
 
-(defvar mythic-bastionland-labeled-feature-types
-  '(myths sanctums monuments dwellings hazards curses ruins holding)
-  "Feature types that are labeled, and thus renameable.")
+    ;; And last, we handle the barriers as they are a bit of a different
+    ;; creature.  We generate them by placing them between two
+    ;; neighboring hexes.
+    ;;
+    ;; I have given special consideration for hexes on the edge of the
+    ;; map; Namely don't create barriers on the edges.  And
+    ;; proportionally reduce the chance of adding a barrier on those
+    ;; edges proportional to the number sides that the hex has on-map
+    ;; neighbors."
+    (let ((omitted-barriers
+            (mapcar
+              (lambda (b)
+                (mythic-bastionland--make-ordered-pair
+                  (car b) (cdr b)))
+                (alist-get 'barriers (alist-get 'omit config)))))
+      (dotimes (i (+ 23 (random 3)))
+        (let ((keep-trying t))
+          (while keep-trying
+            (let* ((coord
+                     (mythic-bastionland--col-row-to-coord
+                       (random 12) (random 12)))
+                    (in-6-chance
+                      (cond
+                        ((member coord '((0 . 0) (11 . 22)))
+                          ;; top-left, bottom-right
+                          2)
+                        ((member coord '((11 . 0) (0 . 22)))
+                          ;; top-right, bottom-right
+                          3)
+                        ((member (car coord) '(0 11))
+                          ;; from or to
+                          4)
+                        ((member (cdr coord) '(0 23))
+                          ;; top of col that is taller; bottom of col
+                          ;; that is shorter
+                          3)
+                        ((member (cdr coord) '(1 22))
+                          ;; top of col that is shorter; bottom of col
+                          ;; that is taller
+                          5)
+                        (t 6))))
+              (when (<= (+ 1 (random 6)) in-6-chance)
+                (progn
+                  (let* ((neighbor
+                           (seq-random-elt
+                             (mythic-bastionland--neighbors coord)))
+                          (pair
+                            (mythic-bastionland--make-ordered-pair
+                              coord neighbor)))
+                    ;; Don't repeat barriers
+                    (when
+                      (not (or (member pair barriers)
+                             (member pair omitted-barriers)))
+                      (progn
+                        (cl-pushnew pair barriers)
+                        (setq keep-trying nil)))))))))))
+
+    ;; PS...make sure we add the locations and barriers to the map.
+    (cl-pushnew `(locations . ,locations) the-map)
+    (cl-pushnew `(barriers . ,barriers) the-map)
+    (setq mythic-bastionland-map the-map)
+    (mythic-bastionland-map-write)))
 
 (defun mythic-bastionland-feature-rename ()
-  "Re-label a feature."
+  "Re-label `mythic-bastionland' feature."
   (interactive)
   (let* ((feature-type
            (intern
              (completing-read "Feature Type: "
-               mythic-bastionland-labeled-feature-types
+               mythic-bastionland-features
                nil t)))
           (features
             (alist-get feature-type (mythic-bastionland-map)))
@@ -216,12 +347,6 @@ coordinates, as expressed in `mythic-bastionland--col-row-to-coord'."
 
 Here lies your working copy of the map.  Glancing at it could provide
 a hint as to the secrets of your game.")
-
-;; (setq mythic-bastionland-map
-;; (mythic-bastionland-map-generate
-;; '(("Tower" . (9 . 3)) ("Castle" . (5 . 7))
-;;         ("Fortress" . (1 . 19)) ("Town" . (8 . 16)))))
-
 
 (defun mythic-bastionland--col-row-to-coord (&optional column row prefix)
   "Prompt for COLUMN and ROW returning a `cons' cell.
@@ -301,54 +426,6 @@ Expected strategies are:
                 (user-error "Unknown stratgegy %s" strategy))))
           distances)))))
 
-(defun mythic-bastionland--generate-barriers ()
-  "Generate barriers placing them between two neighboring hexes.
-
-I have given special consideration for hexes on the edge of the map;
-Namely don't create barriers on the edges.  And proportionally reduce
-the chance of adding a barrier on those edges proportional to the number
-sides that the hex has on-map neighbors."
-  (let ((barriers nil))
-    (dotimes (i (+ 23 (random 3)))
-      (let ((keep-trying t))
-        (while keep-trying
-          (let* ((coord
-                   (mythic-bastionland--col-row-to-coord
-                     (random 12) (random 12)))
-                  (in-6-chance
-                    (cond
-                      ((member coord '((0 . 0) (11 . 22)))
-                        ;; top-left, bottom-right
-                        2)
-                      ((member coord '((11 . 0) (0 . 22)))
-                        ;; top-right, bottom-right
-                        3)
-                      ((member (car coord) '(0 11))
-                        ;; from or to
-                        4)
-                      ((member (cdr coord) '(0 23))
-                        ;; top of col that is taller; bottom of col that
-                        ;; is shorter
-                        3)
-                      ((member (cdr coord) '(1 22))
-                        ;; top of col that is shorter; bottom of col
-                        ;; that is taller
-                        5)
-                      (t 6))))
-            (when (<= (+ 1 (random 6)) in-6-chance)
-              (progn
-                (let* ((neighbor
-                         (seq-random-elt
-                           (mythic-bastionland--neighbors coord)))
-                        (pair
-                          (mythic-bastionland--make-ordered-pair
-                            coord neighbor)))
-                  ;; Don't repeat barriers
-                  (unless (member pair barriers)
-                    (add-to-list 'barriers pair)
-                    (setq keep-trying nil)))))))))
-    barriers))
-
 (defun mythic-bastionland-barrier-between (from to)
   "Answer if there is a barrier goint FROM hex TO hex."
   (interactive
@@ -393,7 +470,7 @@ Neighboring coordinates that are not on the map are omitted."
              (row
                (+ (cdr coord) (cdr delta))))
         (when (and (<= 0 col 11) (<= 0 row 23))
-          (add-to-list 'neighbors (cons col row)))))
+          (cl-pushnew (cons col row) neighbors))))
     neighbors))
 
 (defun mythic-bastionland-direction (from myth)
